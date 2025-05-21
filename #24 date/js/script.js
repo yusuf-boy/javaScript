2d7f1cf1f4cd9f4748f81570b4100763949bb1dc
yusuf-boy <avlioqulovyusuf96@gmail.com>
2025-05-21 17:03:37 +0500

    cod completed

---
+++ #24 date/js/script.js
@@ -0,0 +1,22 @@
+// const  now = new Date();
+//
+// const parse = Date.parse("2001-09-24");
+// // now.setHours(11)
+// console.log(parse);
+// console.log(now)
+//
+// // console.log(now.getHours());
+// // console.log(now.getMinutes());
+//
+// console.log(now.getTimezoneOffset())
+
+
+// let startDate = new Date();
+//
+// for (let i = 0; i < 100000000; i++) {
+//     let number = i ** 3
+// }
+//
+// let endDate = new Date();
+//
+// console.log(`Loop war worked ${endDate - startDate} milliseconds`);
